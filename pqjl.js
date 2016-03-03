278ad846cf45f4e16caffa391c3d8b4cc4679ea2
javilag <jlavil_194@hotmail.com>
2016-03-03 17:50:39 -0500

    ya lleva los datos a la base de datos

--- pqjl.js
+++ pqjl.js
@@ -1,18 +1,26 @@
 var pg = require('pg');
+var conString = "postgres://postgres:password@localhost:5432/RFID";
 
-var conString = "postgres://username:password@localhost/database";
 
-var client = new pg.Client(conString);
-client.connect(function(err) {
+//this initializes a connection pool
+//it will keep idle connections open for a (configurable) 30 seconds
+//and set a limit of 20 (also configurable)
+pg.connect(conString, function(err, client, done) {
   if(err) {
-    return console.error('could not connect to postgres', err);
+    return console.error('error fetching client from pool', err);
   }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
     if(err) {
       return console.error('error running query', err);
     }
-    console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-    client.end();
+    console.log(result.rows[0].number);
+    //output: 1
   });
+  client.query({
+     text: "select ingresar_persona('148','lmao','mas','lul','1234','432','234','1324')"
+     //values: ['134','lmao','mas','lul','1234','432','234','1324'],
+   });
 });
