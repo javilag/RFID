f65b5da707879fd2f89e15f54afdafe3ce0a395a
javilag <PRACTICAS@pc136.salas.local>
2016-03-10 17:56:24 -0500

    se prueban los scope para metodo de inserción

--- RFID/public/javascripts/app.js
+++ RFID/public/javascripts/app.js
@@ -15,4 +15,22 @@ angular.module('RFID', [])
         .error(function(error) {
             console.log('Error: ' + error);
         });
-});
+    });
+    $scope.createPersona = function(personaID) {
+    $http.post('/api/v1/RFID', $scope.formData)
+        .success(function(data) {
+            $scope.formData = {};
+            $scope.doc_id = data.doc_id;
+            $scope.nombre = data.nom;
+            $scope.genero = data.genero;
+            $scope.correo = data.correo;
+            $scope.tel = data.tel;
+            $scope.cel = data.cel;
+            $scope.cod_tarjeta = data.cod_tarjeta;
+            $scope.cod_universidad = data.cod_universidad;
+            console.log(data);
+        })
+        .error(function(error) {
+            console.log('Error: ' + error);
+        });
+};
