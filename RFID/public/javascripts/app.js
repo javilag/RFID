e7baff0e43793ccd9b4e982e58585cb820d635b7
javilag <jlavil_194@hotmail.com>
2016-03-17 00:17:14 -0500

    se comprende mejor como se envía un objeto

--- RFID/public/javascripts/app.js
+++ RFID/public/javascripts/app.js
@@ -2,8 +2,8 @@ var angular = require('angular');
 angular.module('RFID', [])
 
 .controller('mainController', function($scope, $http) {
-
-    $scope.formData = {};
+    $scope.master = {};
+    $scope.persona = {};
     $scope.todoData = {};
 
     // Get all rfid
@@ -15,22 +15,13 @@ angular.module('RFID', [])
         .error(function(error) {
             console.log('Error: ' + error);
         });
-    });
-    $scope.createPersona = function(personaID) {
-    $http.post('/api/v1/RFID', $scope.formData)
-        .success(function(data) {
-            $scope.formData = {};
-            $scope.doc_id = data.doc_id;
-            $scope.nombre = data.nom;
-            $scope.genero = data.genero;
-            $scope.correo = data.correo;
-            $scope.tel = data.tel;
-            $scope.cel = data.cel;
-            $scope.cod_tarjeta = data.cod_tarjeta;
-            $scope.cod_universidad = data.cod_universidad;
-            console.log(data);
-        })
-        .error(function(error) {
-            console.log('Error: ' + error);
-        });
-};
+    $scope.createPersona = function(persona) {
+        $http.post('/api/v1/RFID', $scope.persona)
+            .success(function(data) {
+                console.log(data);
+            })
+            .error(function(error) {
+                console.log('Error: ' + error);
+            });
+    };
+});
