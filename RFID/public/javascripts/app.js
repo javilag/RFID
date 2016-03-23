9aa7f9b556144fcfb46a4431af2cb76f9dc30d4c
Jorge Avila <jlavil_194@hotmail.com>
2016-03-23 01:11:49 -0500

    Se muestran los datos, se intenta eliminar parece indicar que todo esta bien hasta la forma de escribir el query y el where

--- RFID/public/javascripts/app.js
+++ RFID/public/javascripts/app.js
@@ -3,6 +3,16 @@ angular.module('RFID', [])
 
 .controller('mainController', function($scope, $http) {
     $scope.persona = {};
+    $scope.cargaPersona = {};
+    $http.get('/api/v1/RFID')
+        .success(function(data){
+            $scope.cargaPersona  = data;
+            console.log(data);
+        })
+        .error(function(error) {
+            console.log('Error: ' + error);
+        });
+
     $scope.createPersona = function(persona) {
         $http.post('/api/v1/RFID', $scope.persona)
             .success(function(data) {
@@ -14,4 +24,15 @@ angular.module('RFID', [])
                 console.log('Error: ' + error);
             });
     };
+    $scope.deletePersona = function(persona) {
+    $http.delete('/api/v1/RFID')
+        .success(function(data) {
+            $scope.persona  = {};
+            $scope.persona  = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+};
 });
