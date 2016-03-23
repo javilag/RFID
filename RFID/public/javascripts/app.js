8a911713758883eb7c36b397f22964e428fcfe11
Jorge Avila <jlavil_194@hotmail.com>
2016-03-22 22:15:22 -0500

    i hope nothing will change, pls life

--- RFID/public/javascripts/app.js
+++ RFID/public/javascripts/app.js
@@ -1,23 +1,14 @@
-var angular = require('angular');
+
 angular.module('RFID', [])
 
 .controller('mainController', function($scope, $http) {
     $scope.master = {};
     $scope.persona = {};
-    $scope.todoData = {};
-
-    // Get all rfid
-    $http.get('/api/v1/RFID')
-        .success(function(data) {
-            $scope.todoData = data;
-            console.log(data);
-        })
-        .error(function(error) {
-            console.log('Error: ' + error);
-        });
     $scope.createPersona = function(persona) {
         $http.post('/api/v1/RFID', $scope.persona)
             .success(function(data) {
+                $scope.persona  = {};
+                $scope.persona  = data;
                 console.log(data);
             })
             .error(function(error) {
